3fa29c4bb3a35d0afa0768ba9b631ae6fb622efd
Bibek <adhikaribi@cwu.edu>
2024-04-11 23:39:46 -0700

    Implemented inOrderTraversal method

--- TreeDemo.java
+++ TreeDemo.java
@@ -63,6 +63,7 @@ class BinarySearchTree {
 		}
 	}
 
+
 	/**
 	 * Performs an in-order traversal of the binary search tree.
 	 *
@@ -77,6 +78,7 @@ class BinarySearchTree {
 	}
 
 	
+
 	/**
 	 * Performs a post-order traversal of the binary search tree.
 	 *
